fbcb64f370024fac8762e494bddc2d48d8fc1b57
Katarina Vucic <vucic.kat@gmail.com>
2021-10-28 20:58:49 +0200

    obradjene sve rute u index.php

--- index.php
+++ index.php
@@ -30,7 +30,14 @@ Flight::route('GET /novosti', function () {
     return false;
 });
 
-Flight::route('GET /novosti/@id', function () {
+Flight::route('GET /novosti/@id', function ($id) {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    $db->select("novosti", "*", "kategorije", "kategorija_id", "id", "novosti.id = " . $id, null);
+    $red = $db->getResult()->fetch_object();
+    $json_niz = json_encode($red, JSON_UNESCAPED_UNICODE);
+    echo indent($json_niz);
+    return false;
 });
 
 Flight::route('POST /novosti', function () {
@@ -128,8 +135,23 @@ Flight::route('PUT /novosti/@id', function ($id) {
     }
 });
 
-Flight::route('DELETE /novosti/@id', function () {
+Flight::route('DELETE /novosti/@id', function ($id) {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    if ($db->delete("novosti", array("id"), array($id))) {
+        $odgovor["poruka"] = "Novost je uspešno izbrisana";
+        $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+        echo $json_odgovor;
+        return false;
+    } else {
+        $odgovor["poruka"] = "Došlo je do greške prilikom brisanja novosti";
+        $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+        echo $json_odgovor;
+        return false;
+    }
 });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 Flight::route('GET /kategorije', function () {
     header("Content-Type: application/json; charset=utf-8");
@@ -153,15 +175,113 @@ Flight::route('GET /kategorije', function () {
     return false;
 });
 
-Flight::route('GET /kategorije/@id', function () {
+Flight::route('GET /kategorije/@id', function ($id) {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    $db->select("kategorije", "*", null, null, null, "kategorije.id = " . $id, null);
+    $niz = array();
+
+    $red = $db->getResult()->fetch_object();
+
+    $niz["id"] = $red->id;
+    $niz["kategorija"] = $red->kategorija;
+    $db_pomocna = new Database("rest");
+    $db_pomocna->select("novosti", "*", null, null, null, "novosti.kategorija_id = " . $red->id, null);
+    while ($red_pomocna = $db_pomocna->getResult()->fetch_object()) {
+        $niz["novosti"][] = $red_pomocna;
+    }
+
+    $json_niz = json_encode($niz, JSON_UNESCAPED_UNICODE);
+    echo indent($json_niz);
+    return false;
 });
 
 Flight::route('POST /kategorije', function () {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    $podaci_json = Flight::get("json_podaci");
+    $podaci = json_decode($podaci_json);
+    if ($podaci == null) {
+        $odgovor["poruka"] = "Niste prosledili podatke";
+        $json_odgovor = json_encode($odgovor);
+        echo $json_odgovor;
+    } else {
+        if (!property_exists($podaci, 'kategorija')) {
+            $odgovor["poruka"] = "Niste prosledili korektne podatke";
+            $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+            echo $json_odgovor;
+            return false;
+        } else {
+            $podaci_query = array();
+            foreach ($podaci as $k => $v) {
+                $v = "'" . $v . "'";
+                $podaci_query[$k] = $v;
+            }
+            if ($db->insert("kategorije", "kategorija", array($podaci_query["kategorija"]))) {
+                $odgovor["poruka"] = "Kategorija je uspešno ubačena";
+                $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+                echo $json_odgovor;
+                return false;
+            } else {
+                $odgovor["poruka"] = "Došlo je do greške pri ubacivanju novosti";
+                $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+                echo $json_odgovor;
+                return false;
+            }
+        }
+    }
 });
 
-Flight::route('PUT /kategorije/@id', function () {
+Flight::route('PUT /kategorije/@id', function ($id) {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    $podaci_json = Flight::get("json_podaci");
+    $podaci = json_decode($podaci_json);
+    if ($podaci == null) {
+        $odgovor["poruka"] = "Niste prosledili podatke";
+        $json_odgovor = json_encode($odgovor);
+        echo $json_odgovor;
+    } else {
+        if (!property_exists($podaci, 'kategorija')) {
+            $odgovor["poruka"] = "Niste prosledili korektne podatke";
+            $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+            echo $json_odgovor;
+            return false;
+        } else {
+            $podaci_query = array();
+            foreach ($podaci as $k => $v) {
+                $v = "'" . $v . "'";
+                $podaci_query[$k] = $v;
+            }
+            if ($db->update("kategorije", $id, array('kategorija'), array($podaci->kategorija))) {
+                $odgovor["poruka"] = "Kategorija je uspešno izmenjena";
+                $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+                echo $json_odgovor;
+                return false;
+            } else {
+                $odgovor["poruka"] = "Došlo je do greške pri izmeni kategorije";
+                $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+                echo $json_odgovor;
+                return false;
+            }
+        }
+    }
 });
 
-Flight::route('DELETE /kategorije/@id', function () {
+Flight::route('DELETE /kategorije/@id', function ($id) {
+    header("Content-Type: application/json; charset=utf-8");
+    $db = Flight::db();
+    if ($db->delete("kategorije", array("id"), array($id))) {
+        $odgovor["poruka"] = "Kategorija je uspešno izbrisana";
+        $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+        echo $json_odgovor;
+        return false;
+    } else {
+        $odgovor["poruka"] = "Došlo je do greške prilikom brisanja kategorije";
+        $json_odgovor = json_encode($odgovor, JSON_UNESCAPED_UNICODE);
+        echo $json_odgovor;
+        return false;
+    }
 });
+
 Flight::start();
